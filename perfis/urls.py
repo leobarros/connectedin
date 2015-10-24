5556e467016a453870c68f9067377b476935e3cd
Leonardo Sales <lsales@globosat.com.br>
2015-10-24 17:24:44 -0200

    Aula 6: Convidando perfis

--- perfis/urls.py
+++ perfis/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, include, url
+from perfis import views
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^$', 'perfis.views.index', name='index'),
-    url(r'^perfis/(?P<perfil_id>\d+)$', 'perfis.views.exibir', name='exibir')
+    url(r'^$', views.index, name='index'),
+    url(r'^perfis/(?P<perfil_id>\d+)$', views.exibir, name='exibir'),
+    url(r'^perfis/(?P<perfil_id>\d+)/convidar$', views.convidar, name='convidar')
 )
