f6621991836666783d02d12675d0d0f226e4b69a
Leonardo Barros <nickolback@gmail.com>
2016-01-31 17:18:26 -0200

    Cadastro de usuários

--- perfis/urls.py
+++ perfis/urls.py
@@ -3,8 +3,8 @@ from perfis import views
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^perfis/(?P<perfil_id>\d+)$', views.exibir, name='exibir'),
-    url(r'^perfis/(?P<perfil_id>\d+)/convidar$', views.convidar, name='convidar'),
-    url(r'^convite/(?P<convite_id>\d+)/aceitar$', views.aceitar, name='aceitar')
-)
+                       url(r'^$', views.index, name='index'),
+                       url(r'^perfis/(?P<perfil_id>\d+)$', views.exibir, name='exibir'),
+                       url(r'^perfis/(?P<perfil_id>\d+)/convidar$', views.convidar, name='convidar'),
+                       url(r'^convite/(?P<convite_id>\d+)/aceitar$', views.aceitar, name='aceitar')
+                       )
